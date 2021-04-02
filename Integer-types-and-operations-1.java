ec6e88fee3ef85ccd703224d221876b46fefad20
chayapalin <68495962+chayapalin@users.noreply.github.com>
2021-04-02 07:46:57 +0700

    Create Integer-types-and-operations-1.java

---
+++ Integer-types-and-operations-1.java
@@ -0,0 +1,19 @@
+Given a two-digit number. Print its first digit (i.e. the number of tens).
+
+Sample Input 1:
+42
+
+Sample Output 1:
+4
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int result = num / 10;
+
+        System.out.println(result);
+    }
+}
