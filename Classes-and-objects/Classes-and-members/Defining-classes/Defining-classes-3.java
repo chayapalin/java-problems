2ae6dce6dd178161f6fdd3b214b23951d29745a1
chayapalin <68495962+chayapalin@users.noreply.github.com>
2021-04-07 20:55:59 +0700

    Add files via upload

---
+++ Classes-and-objects/Classes-and-members/Defining-classes/Defining-classes-3.java
@@ -0,0 +1,12 @@
+// Define a class named Box. It must have four fields:
+// length, height and width of the double type
+// a field named innerBox of the same type as the defined class
+//
+// Do not make the fields private.
+
+class Box {
+    double length;
+    double height;
+    double width;
+    Box innerBox;
+}
